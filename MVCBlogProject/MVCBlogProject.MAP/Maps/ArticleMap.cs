c462333494a585f606d1a7931a23b842e94efeca
ertglr.61 <ertglr.61@outlook.com.tr>
2019-10-27 13:18:19 +0300

    Map Katmanı oluşturuldu ve tamamlandı.

---
+++ MVCBlogProject/MVCBlogProject.MAP/Maps/ArticleMap.cs
@@ -0,0 +1,21 @@
+﻿using MVCBlogProject.CORE.Map;
+using MVCBlogProject.MODEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBlogProject.MAP.Maps
+{
+    public class ArticleMap:CoreMap<Article>
+    {
+        public ArticleMap()
+        {
+            ToTable("dbo.Articles");
+            Property(i => i.Header).IsRequired();
+            Property(i => i.Description).IsRequired();
+            Property(i => i.Content).IsRequired();
+        }
+    }
+}
