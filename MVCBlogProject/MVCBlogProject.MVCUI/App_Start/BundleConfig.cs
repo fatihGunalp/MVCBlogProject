90ff68a441e56ec2b5fbf5ec5e39461cabd2e9b0
Ömer Faruk Bal <omerbal@gmail.com>
2019-10-28 12:54:45 +0300

    BaseService içerisinde context tanımlandı, syntax hataları giderildi. Roles isimli enuma Editor eklemesi yapıldı. Admin panel için tema eklendi ve gerekli entegrasyonlar uygulandı. Menü düzenlendi. Bundles eklendi. Controllerlar oluşturuldu. Viewler oluşturuldu.

--- MVCBlogProject/MVCBlogProject.MVCUI/App_Start/BundleConfig.cs
+++ MVCBlogProject/MVCBlogProject.MVCUI/App_Start/BundleConfig.cs
@@ -25,6 +25,33 @@ namespace MVCBlogProject.MVCUI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Admin/css").Include(
+                      "~/Content/AdminTheme/css/bootstrap.min.css",
+                      "~/Content/AdminTheme/css/bootstrap-reset.css",
+                      "~/Content/AdminTheme/assets/font-awesome/css/font-awesome.css",
+                      "~/Content/AdminTheme/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css",
+                      "~/Content/AdminTheme/css/owl.carousel.css",
+                      "~/Content/AdminTheme/css/style.css",
+                      "~/Content/AdminTheme/css/style-responsive.css"));
+
+            bundles.Add(new ScriptBundle("~/Admin/js").Include(
+                      "~/Content/AdminTheme/js/jquery.js",
+                      "~/Content/AdminTheme/js/jquery-1.8.3.min.js",
+                      "~/Content/AdminTheme/js/bootstrap.min.js",
+                      "~/Content/AdminTheme/js/jquery.dcjqaccordion.2.7.js",
+                      "~/Content/AdminTheme/js/jquery.scrollTo.min.js",
+                      "~/Content/AdminTheme/js/jquery.nicescroll.js",
+                      "~/Content/AdminTheme/js/jquery.sparkline.js",
+                      "~/Content/AdminTheme/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js",
+                      "~/Content/AdminTheme/js/owl.carousel.js",
+                      "~/Content/AdminTheme/js/jquery.customSelect.min.js",
+                      "~/Content/AdminTheme/js/respond.min.js",
+                      "~/Content/AdminTheme/js/jquery.dcjqaccordion.2.7.js",
+                      "~/Content/AdminTheme/js/common-scripts.js",
+                      "~/Content/AdminTheme/js/sparkline-chart.js",
+                      "~/Content/AdminTheme/js/easy-pie-chart.js",
+                      "~/Content/AdminTheme/js/count.js"));
         }
     }
 }
