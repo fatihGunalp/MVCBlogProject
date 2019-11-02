b963bd9be7433236b970f7200170e1677baeb527
oguzcan ocal <oguzcanocal.34@gmail.com>
2019-11-02 13:35:20 +0300

    Comment alanında listeleme ve ekleme bölümleri eklendi.

--- MVCBlogProject/MVCBlogProject.MODEL/Entities/Comment.cs
+++ MVCBlogProject/MVCBlogProject.MODEL/Entities/Comment.cs
@@ -1,6 +1,7 @@
 ﻿using MVCBlogProject.CORE.Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace MVCBlogProject.MODEL.Entities
 {
     public class Comment : CoreEntity
     {
+        [Display(Name = "Başlık")]
         public string Header { get; set; }
+        [Display(Name = "Yazı")]
         public string Text { get; set; }
+        [Display(Name = "Yazar")]
         public string Owner { get; set; }
 
 
--- MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Controllers/CommentController.cs
+++ MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Controllers/CommentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVCBlogProject.MODEL.Entities;
+using MVCBlogProject.SERVICE.Option;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,35 @@ namespace MVCBlogProject.MVCUI.Areas.Admin.Controllers
     public class CommentController : Controller
     {
         // GET: Admin/Comment
+        CommentService cs = new CommentService();
         public ActionResult Index()
+        {
+            return View(cs.GetAll());
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Comment model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ID = Guid.NewGuid();
+                cs.Add(model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+
     }
 }
