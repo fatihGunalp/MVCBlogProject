27f2bfc62a8d1dc8e2db729459421988c5a35278
Salih Genç <salihgenc34@gmail.com>
2019-11-02 13:59:22 +0300

    Etiket Crud işlemlerine başladı.

--- MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Controllers/TagController.cs
+++ MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Controllers/TagController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MVCBlogProject.MODEL.Entities;
+using MVCBlogProject.MVCUI.Areas.Admin.Models;
+using MVCBlogProject.SERVICE.Option;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,49 @@ namespace MVCBlogProject.MVCUI.Areas.Admin.Controllers
 {
     public class TagController : Controller
     {
+        TagService tagnames = new TagService();
         // GET: Admin/Tag
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Tag data)
+        {
+            data.ID = Guid.NewGuid();
+            tagnames.Add(data);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Update(Guid id)
+        {
+            TagCategoryVM model = new TagCategoryVM();
+            model.TagCategories = tagnames.GetById(id);
+            //model.TagCategories = category.GetActive();
+            return View(model);
+
+
+
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Update(SubCategory model)
+        {
+            //sub.Update(model);
+            //return RedirectToAction("Index");
+        }
+        public ActionResult Delete(Guid id)
+        {
+            //var deleted = sub.GetById(id);
+            //sub.Remove(deleted);
+            //return RedirectToAction("Index");
         }
     }
 }
---
+++ MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Models/TagCategoryVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlogProject.MVCUI.Areas.Admin.Models
+{
+    public class TagCategoryVM
+    {
+        public TagCategoryVM TagCategories { get; set; }
+
+    }
+}
