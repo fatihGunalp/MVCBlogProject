0a351279a85dff8b4f4f5b8a25beffd6c2ce84a4
ertglr.61 <ertglr.61@outlook.com.tr>
2019-11-02 13:56:55 +0300

    Admin tarafında üye ekleme işlemleri yapıldı güncelleme silme vs işlemleri için getByid bekleniyor.

--- MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Controllers/UserController.cs
+++ MVCBlogProject/MVCBlogProject.MVCUI/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using MVCBlogProject.MODEL.Entities;
+using MVCBlogProject.SERVICE.Option;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +11,65 @@ namespace MVCBlogProject.MVCUI.Areas.Admin.Controllers
     public class UserController : Controller
     {
         //todo: UserService oluşturulduktan sonra aşağıdaki actionların işlemleri tamamlanacak.
+        UserService db = new UserService();
         public ActionResult Index()
         {
-            return View();
+            return View(db.GetAll());
         }
 
         public ActionResult Create()
         {
+            
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(User model)
+        public ActionResult Create(User model, HttpPostedFileBase picture)
         {
-            return RedirectToAction("Index");
+            try
+            {
+
+                if (picture != null)
+                {
+                    string pictureName = System.IO.Path.GetFileName(picture.FileName);
+                    string pictureAdress = Server.MapPath("~/Uploads/" + pictureName);
+
+                    picture.SaveAs(pictureAdress);
+
+                    //var resim = Request.Form["ImagePath"];
+                    model.ImagePath = pictureAdress;
+
+
+
+                }
+
+                model.ID = Guid.NewGuid();
+                if (ModelState.IsValid)
+                {
+                    db.Add(model);
+                    return RedirectToAction("Index");
+                }
+
+               
+                return View(model);
+            }
+            catch (Exception e)
+            {
+
+                ViewBag.Error = e.InnerException;
+                return View(model);
+            }
+           
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(Guid ID)
         {
+            //service de getByid eklenmesi bekleniyor.
             return View();
         }
 
         [HttpPost]
-        public ActionResult Edit(Guid Id)
+        public ActionResult Edit(Guid Id,HttpPostedFileBase picture)
         {
             return RedirectToAction("Index");
         }
