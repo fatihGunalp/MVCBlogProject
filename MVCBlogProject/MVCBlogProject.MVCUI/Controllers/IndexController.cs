dbdca83af450bef2dd29c272a67d963444051deb
fatih günalp <fatih.gunalp@outlook.com>
2019-11-03 13:52:28 +0300

    Revert "UI Tarafında Kategori Çekme - Anasayfa Düzenleme İşlemleri-Kayıt Ol Bölümü Eklendi"
    This reverts commit d67024ece556b3598f3d11f6fef7ac0d1fb4ee44.

--- MVCBlogProject/MVCBlogProject.MVCUI/Controllers/IndexController.cs
+++ MVCBlogProject/MVCBlogProject.MVCUI/Controllers/IndexController.cs
@@ -1,5 +1,4 @@
-﻿using MVCBlogProject.SERVICE.Option;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,16 +9,8 @@ namespace MVCBlogProject.MVCUI.Controllers
     public class IndexController : Controller
     {
         // GET: Index
-        public PartialViewResult Menu()
-        {
-            CategoryService ctg = new CategoryService();
-          var menu=  ViewBag.Menu = ctg.GetAll();
-            return PartialView("_HeaderPartial",menu);
-        }
         public ActionResult Homepage()
         {
-            CategoryService ctg = new CategoryService();
-            ViewBag.Menu = ctg.GetAll();
             return View();
         }
 
