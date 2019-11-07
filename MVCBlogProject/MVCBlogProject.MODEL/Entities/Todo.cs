07e9802488e6eb057192706e61220b8cb522f366
Ömer Faruk Bal <omerbal@gmail.com>
2019-11-07 10:18:28 +0300

    Todo adında Entity oluşturuldu. Aynı şekilde map ve service ler oluşturuldu. DbContext işlemleri yapıldı. Todo controller oluşturuldu. Todo sistemi yapıldı.

---
+++ MVCBlogProject/MVCBlogProject.MODEL/Entities/Todo.cs
@@ -0,0 +1,21 @@
+﻿using MVCBlogProject.CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBlogProject.MODEL.Entities
+{
+    public class Todo : CoreEntity
+    {
+        [Required(ErrorMessage = "Görev detayı girmelisiniz.")]
+        public string TaskContent { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Guid UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
